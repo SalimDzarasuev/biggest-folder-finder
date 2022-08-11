c2c141076780417266ff320e69ca5bdb836ecc69
Dzarasuev Salim <sdzarasuev@gmail.com>
2022-08-11 14:05:52 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import java.io.File;
+
 public class Main {
+    public static void main(String[] args) {
+        String folderFile = "C:\\Users\\VTB\\Desktop";
+        File file = new File(folderFile);
+
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder){
+        if (folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File [] files = folder.listFiles();
+        for (File file : files){
+            sum += getFolderSize(file);
+        }
+        return sum;
+
+    }
 }
